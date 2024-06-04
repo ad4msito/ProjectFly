d2ba97ecc65c23aae7874bdb59ad055a07919a92
Adam <129022806+ad4msito@users.noreply.github.com>
2024-06-04 15:10:37 -0300

    Delete server.js

--- server.js
+++
@@ -1,7 +0,0 @@
-const app = require('./app');
-require('dotenv').config();
-const port = process.env.PORT || 8078;
-
-app.listen(port, () => {
-  console.log('Servidor escuchando en el puerto ' + port);
-});
