55be73757795eef6042fbcbfda89b2b8f07fcd20
Adam <129022806+ad4msito@users.noreply.github.com>
2024-06-04 15:10:02 -0300

    Delete controller directory

--- controller/vuelo/router.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const vueloController = require('./controller');
-
-router.get('/',vueloController.getVuelos);
-router.get('/:id',vueloController.getOne);
-module.exports = router;
